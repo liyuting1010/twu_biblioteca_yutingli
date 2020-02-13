3f0bbd4747411e974a739c2f406005497448ccaa
liyuting1010 <liyuting1010@163.com>
2020-02-13 18:50:33 +0800

    Initial spring project.

---
+++ src/main/java/com/twu/bibiloteca/BibliotecaApp.java
@@ -0,0 +1,15 @@
+package com.twu.bibiloteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+
+        SpringApplication.run(BibliotecaApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,11 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BibliotecaAppTest {
+    @Test public void testAppHasAGreeting() {
+        assertEquals(1, 1);
+    }
+}
