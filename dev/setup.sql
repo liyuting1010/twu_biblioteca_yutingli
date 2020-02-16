cc061bf052608d395dab318342278bd62caf4b70
liyuting1010 <liyuting951010@163.com>
2020-02-16 15:46:00 +0800

    Add users table.

--- dev/setup.sql
+++ dev/setup.sql
@@ -13,3 +13,18 @@ VALUES
 ('Glimpses of World History', 'Jawaharlal Nehru', 4),
 ('Prison Diary', 'Jay Prakash Narayan', 1),
 ('Why Socialism', 'Jayaprakash Narayan', 0);
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users
+(
+  id       SERIAL PRIMARY KEY,
+  name     VARCHAR(250) NOT NULL,
+  password VARCHAR(250)
+);
+
+INSERT INTO users(name, password)
+VALUES ('Yuting1', 'password1'),
+       ('Yuting2', 'password2'),
+       ('Yuting3', 'password3');
+
