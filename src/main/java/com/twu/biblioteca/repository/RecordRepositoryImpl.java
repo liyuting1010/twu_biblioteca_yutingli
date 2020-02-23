06ce6b55f464ae381fd78802ed1cac31f854b878
liyuting1010 <liyuting951010@163.com>
2020-02-23 19:09:07 +0800

    Remove userId from record.

--- src/main/java/com/twu/biblioteca/repository/RecordRepositoryImpl.java
+++ src/main/java/com/twu/biblioteca/repository/RecordRepositoryImpl.java
@@ -21,13 +21,12 @@ public class RecordRepositoryImpl implements RecordRepository {
     @Override
     public List<Record> getRecordsById(Integer userId) {
         List<Record> recordList = new ArrayList<>();
-        try (final PreparedStatement statement = dbConnection.prepareStatement("SELECT books.id, books.name, borrow_records.uid,  borrow_records.borrow_date, borrow_records.return_date FROM borrow_records JOIN books ON books.id = borrow_records.bid WHERE uid = ?")) {
+        try (final PreparedStatement statement = dbConnection.prepareStatement("SELECT books.id, books.name, borrow_records.borrow_date, borrow_records.return_date FROM borrow_records JOIN books ON books.id = borrow_records.bid WHERE uid = ?")) {
             statement.setInt(1, userId);
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
                 Record record = new Record(
                         resultSet.getInt("id"),
-                        resultSet.getInt("uid"),
                         resultSet.getString("name"),
                         resultSet.getTimestamp("borrow_date"),
                         resultSet.getTimestamp("return_date")
--- src/main/java/com/twu/biblioteca/users/records/Record.java
+++ src/main/java/com/twu/biblioteca/users/records/Record.java
@@ -4,14 +4,12 @@ import java.sql.Timestamp;
 
 public class Record {
     private final Integer bookId;
-    private final Integer userId;
     private final String bookName;
     private final Timestamp borrowDate;
     private final Timestamp returnDate;
 
-    public Record(Integer bookId, Integer userId, String bookName, Timestamp borrowDate, Timestamp returnDate) {
+    public Record(Integer bookId, String bookName, Timestamp borrowDate, Timestamp returnDate) {
         this.bookId = bookId;
-        this.userId = userId;
         this.bookName = bookName;
         this.borrowDate = borrowDate;
         this.returnDate = returnDate;
@@ -19,10 +17,6 @@ public class Record {
 
     public Integer getBookId() {
         return bookId;
-    }
-
-    public Integer getUserId() {
-        return userId;
     }
 
     public String getBookName() {
